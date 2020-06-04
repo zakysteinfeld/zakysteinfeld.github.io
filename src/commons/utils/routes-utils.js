321438bef393726cf26676ca9c9fd691462d4450
zakysteinfeld <51196879+zakysteinfeld@users.noreply.github.com>
2020-06-04 19:23:31 +0900

    Revert "coba"

---
+++ src/commons/utils/routes-utils.js
@@ -0,0 +1,4 @@
+export const DEV_URI = 'http://localhost:3000';
+export const PROD_URI = 'https://zakysteinfeld.github.io/portfolio';
+export const DEV_ENV = '/';
+export const PROD_ENV = '/portfolio/';
